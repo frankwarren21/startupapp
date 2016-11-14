4c72accdd6b4393c758de9b50fd4a3c4b8e3fe45
frankwarren21 <FLacko@Franks-MacBook-Air-2.local>
2016-11-14 10:37:06 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+ruby "2.2.5"
+gem 'sinatra', '1.4.7'
---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'rubygems'
+require 'twilio-ruby'
+require 'sinatra'
+
+
+
+get '/' do
+erb :index
+end
+
+  account_sid = 'AC318322dd6f08156292077d626b8850db'
+  auth_token = 'd380ee4c115dafb11f5938fb059db58b'
+post '/send' do
+
+  @client = Twilio::REST::Client.new(account_sid, auth_token)
+  to_number = params.fetch('number').delete('.-').to_i
+
+  @client.account.messages.create({
+    :from => '+19179094316',
+    :to => to_number,
+    :body => 'WORKING',
+  })
+end
